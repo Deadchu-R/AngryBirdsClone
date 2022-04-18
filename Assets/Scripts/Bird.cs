a202cb699d51ec96759aadbd94885b58c4772376
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-04-18 18:49:38 +0300

    Bird Script Update

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,7 +7,7 @@ public class Bird : MonoBehaviour
     private Vector3 screenPoint;
     private Vector3 offset;
     private Vector3 dragStartPos;
-    private Vector3 dragEndPos;
+    public Vector3 dragEndPos;
     private float dragDistance; // updates every frame
     [SerializeField]
     private float maxDragDistance = 2;
@@ -18,16 +18,105 @@ public class Bird : MonoBehaviour
     [SerializeField]
     private float forceFactorY = 500;
 
+    [Header("Line renderer veriables")]
+    public LineRenderer line;
+    [Range(2, 30)]
+    public int resolution;
+
+    [Header("Formula variables")]
+    public Vector2 velocity;
+    public float yLimit;
+    private float g;
+
+    [Header("Linecast variables")]
+    [Range(2, 30)]
+    public int linecastResolution;
+    public LayerMask canHit;
+
+    // Bird > bird fly > bird speed  >
+    //  CurPos 
+    // if -2 > CurTransform.pos.x > 2 || -2 > CurTransform.pos.y > 2 cant move bird
     // Start is called before the first frame update
     void Start()
     {
+       // gameObject
         rigid = GetComponent<Rigidbody2D>();
-    }
+        g = Mathf.Abs(Physics2D.gravity.y);
 
+    }
     private void Update()
     {
-        
+        velocity.x = -dragEndPos.x * dragDistance  ; velocity.y  = -dragEndPos.y * dragDistance ;
+        RenderArc();
     }
+    private void RenderArc()
+    {
+        line.positionCount = resolution + 1;
+        line.SetPositions(CalculateLineArray());
+    }
+
+    private Vector3[] CalculateLineArray()
+    {
+        Vector3[] lineArray = new Vector3[resolution + 1];
+
+        var lowestTimeValue = MaxTimeX() / resolution;
+
+        for (int i = 0; i < lineArray.Length; i++)
+        {
+            var t = lowestTimeValue * i;
+            lineArray[i] = CalculateLinePoint(t);
+        }
+
+        return lineArray;
+    }
+
+    private Vector2 HitPosition()
+    {
+        var lowestTimeValue = MaxTimeY() / linecastResolution;
+
+        for (int i = 0; i < linecastResolution + 1; i++)
+        {
+            var t = lowestTimeValue * i;
+            var tt = lowestTimeValue * (i + 1);
+
+            var hit = Physics2D.Linecast(CalculateLinePoint(t), CalculateLinePoint(tt), canHit);
+
+            if (hit)
+                return hit.point;
+        }
+
+        return CalculateLinePoint(MaxTimeY());
+    }
+
+    private Vector3 CalculateLinePoint(float t)
+    {
+        float x = velocity.x * t ;
+        float y = (velocity.y * t ) - (g * Mathf.Pow(t, 2) / 2) ;
+        return new Vector3(x + transform.position.x, y + transform.position.y);
+    }
+
+    private float MaxTimeY()
+    {
+        var v = velocity.y;
+        var vv = v * v;
+
+        var t = (v + Mathf.Sqrt(vv + 2 * g * (transform.position.y - yLimit))) / g;
+        return t;
+    }
+
+    private float MaxTimeX()
+    {
+        var x = velocity.x;
+        if (x == 0)
+        {
+            velocity.x = 000.1f;
+            x = velocity.x;
+        }
+
+        var t = (HitPosition().x - transform.position.x) / x;
+        return t;
+    }
+
     private void OnMouseDown()
     {
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
@@ -52,6 +141,7 @@ public class Bird : MonoBehaviour
             curPosition = dragStartPos + (dragVector.normalized * maxDragDistance);
         }
         transform.position = curPosition;
+        
     }
 
     private void OnMouseUp()
